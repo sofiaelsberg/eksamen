308113cf1553cf39d157e84d8664b2ecbd4053ab
MortenBonderup <morten2good@gmail.com>
2020-05-28 15:03:39 +0200

    Final version

---
+++ js/script.js
@@ -0,0 +1,114 @@
+"use strict";
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+document.getElementById("beregn").addEventListener("click", function () {
+    const vurderingsplacering = document.getElementById("vurdering");
+    const resultat = document.getElementById("vurdering");
+
+    const vaegt = Number(document.getElementById("vaegt").value);
+    const hoejde = Number(document.getElementById("hoejde").value);
+    const bmi = vaegt / (hoejde * hoejde);
+
+    resultat.value = bmi.toLocaleString("da-DK", {
+        maximumFractionDigits: 2
+    });
+
+    if (bmi < 18.5) {
+        vurderingsplacering.innerHTML = "Din idelle størrelse er: <p>S</p>";
+    } else if (bmi >= 18.5 && bmi < 25) {
+        vurderingsplacering.innerHTML = "Din idelle størrelse er: <p>M</p>";
+    } else if (bmi >= 25 && bmi < 30) {
+        vurderingsplacering.innerHTML = "Din idelle størrelse er: <p>L</p>";
+    } else if (bmi >= 30) {
+        vurderingsplacering.innerHTML = "Vurdering: <p>Du er svært overvægtig</p>";
+    }
+
+}, false);
+
+document.getElementById("smal").addEventListener("click", function (e) {
+    const kropsform = document.getElementById("kropsform");
+    kropsform.value = "Smal"
+}, false);
+
+document.getElementById("almindelig").addEventListener("click", function (e) {
+    const kropsform = document.getElementById("kropsform");
+    kropsform.value = "Almindelig"
+}, false);
+
+document.getElementById("stor").addEventListener("click", function (e) {
+    const kropsform = document.getElementById("kropsform");
+    kropsform.value = "Stor"
+}, false);
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
